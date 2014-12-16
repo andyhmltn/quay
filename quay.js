6565840950b895067128263fe7dc8e43cf445747
Andy Hamilton <andyhmltn@gmail.com>
2014-12-16 17:31:33 +0000

    Quay is now compatible with AMD/RequireJS

--- quay.js
+++ quay.js
@@ -1,95 +1,95 @@
-(function(){
-	var Quay = function() {
-		var _self = this
+var Quay = function() {
+	var _self = this
 
-		/*Utility Functions*/
-		var contains = function(a, obj) {
-			var i = a.length
-			while(i--) {
-				if (a[i] === obj) {
-					return true
-				}
-			}
-			return false
-		},
-			remove = function(a, x) {
-				if (contains(a,x)) {
-					a.splice(a.indexOf(x),1 )
-				}
-			}
-
-		this.bindings = {}
-
-		var pressing = [],
-			special_keys = {
-				8: "backspace", 9: "tab", 10: "return", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
-				20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
-				37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del",
-				96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
-				104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/",
-				112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8",
-				120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 186: "", 191: "/",
-				220: "\\", 222: "'", 224: "meta"
-			}
-
-		this.convert = function(key) {
-
-			if (contains(Object.keys(special_keys),String(key)))
-			{
-				return special_keys[key]
-			} else {
-				return String.fromCharCode(key).toLowerCase()
+	/*Utility Functions*/
+	var contains = function(a, obj) {
+		var i = a.length
+		while(i--) {
+			if (a[i] === obj) {
+				return true
 			}
 		}
-		this.run = function(key) {
-			var string = this.convert(key.which)
-			pressing.push(string)
-
-			pressing.sort()
-		}
-
-		this.press = function(key_bindings) {
-
-			var keys = Object.keys(key_bindings)
-
-			for(var i=0; i<keys.length; i++) {
-				var sorted = keys[i].split('_').sort().join('_')
-
-				this.bindings[sorted] = key_bindings[keys[i]]
+		return false
+	},
+		remove = function(a, x) {
+			if (contains(a,x)) {
+				a.splice(a.indexOf(x),1 )
 			}
 		}
 
-		this.VERSION = {
-			MAJOR:1,
-			MINOR:0,
-			PATCH:0,
-			FULL :function() {
-				return this.MAJOR+'.'+this.MINOR+'.'+this.PATCH
-			}
+	this.bindings = {}
+
+	var pressing = [],
+		special_keys = {
+			8: "backspace", 9: "tab", 10: "return", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
+			20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
+			37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del",
+			96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
+			104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/",
+			112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8",
+			120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 186: "", 191: "/",
+			220: "\\", 222: "'", 224: "meta"
 		}
 
-		this.keydown = function(e) {
-			_self.run(e)
+	this.convert = function(key) {
 
-			var sorted_pressing    = pressing.sort()
-			var currently_pressing = sorted_pressing.join('_')
-
-			if (contains(Object.keys(_self.bindings),currently_pressing))
-			{
-				_self.bindings[currently_pressing](currently_pressing)
-
-				pressing = []
-			}
+		if (contains(Object.keys(special_keys),String(key)))
+		{
+			return special_keys[key]
+		} else {
+			return String.fromCharCode(key).toLowerCase()
 		}
+	}
+	this.run = function(key) {
+		var string = this.convert(key.which)
+		pressing.push(string)
 
-		this.keyup = function(e) {
-			remove(pressing,_self.convert(e.which))
+		pressing.sort()
+	}
+
+	this.press = function(key_bindings) {
+
+		var keys = Object.keys(key_bindings)
+
+		for(var i=0; i<keys.length; i++) {
+			var sorted = keys[i].split('_').sort().join('_')
+
+			this.bindings[sorted] = key_bindings[keys[i]]
 		}
 	}
 
+	this.VERSION = {
+		MAJOR:1,
+		MINOR:0,
+		PATCH:0,
+		FULL :function() {
+			return this.MAJOR+'.'+this.MINOR+'.'+this.PATCH
+		}
+	}
 
+	this.keydown = function(e) {
+		_self.run(e)
 
-	window.Quay = new Quay()
-	window.onkeydown = window.Quay.keydown
-	window.onkeyup   = window.Quay.keyup
-})()
+		var sorted_pressing    = pressing.sort()
+		var currently_pressing = sorted_pressing.join('_')
+
+		if (contains(Object.keys(_self.bindings),currently_pressing))
+		{
+			_self.bindings[currently_pressing](currently_pressing)
+
+			pressing = []
+		}
+	}
+
+	this.keyup = function(e) {
+		remove(pressing,_self.convert(e.which))
+	}
+}
+
+if ( typeof define == "function" && define.amd) {
+	define([], function() { return Quay; })
+}
+
+window.Quay = new Quay()
+window.onkeydown = window.Quay.keydown
+window.onkeyup   = window.Quay.keyup
