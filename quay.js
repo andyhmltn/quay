86e8558b4fbb0c8ea1075697958958d6ec6a3ddb
Andy Hamilton <andyhmltn@gmail.com>
2015-02-01 14:36:00 +0100

    Rewrote all-the-things to use prototypes and refactored existing code
    This commit is pretty huge as it replaces every line in quay.js. The reason for this is because
    originally Quay was just a hack to get something I was working on done quickly. I've been meaning
    to rewrite it to use prototypes and clean up the code for a long time. This commit doesn't change any
    functionality at all but refactors all-the-codes.
    
    Also to note:
    	* Array.prototype.filter isn't supported in <IE9 so I've added a fallback to keep backwards
    compatibility.

--- quay.js
+++ quay.js
@@ -1,117 +1,146 @@
 var Quay = function($el) {
-	var _self = this
+	var self = this;
 
-	/*Utility Functions*/
-	var contains = function(a, obj) {
-		var i = a.length
-		while(i--) {
-			if (a[i] === obj) {
-				return true
-			}
-		}
-		return false
-	}
+	self.bindings = {};
+	self.pressing = [];
+	self.special_keys = {
+		8: "backspace", 9: "tab", 10: "return", 13: "return", 16: "shift", 17: "ctrl", 91: "cmd", 18: "alt", 19: "pause",
+		20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
+		37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del",
+		96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
+		104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/",
+		112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8",
+		120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 186: "", 191: "/",
+		220: "\\", 222: "'", 224: "cmd", 93: "cmd"
+	};
 
-	this.bindings = {}
-
-	var pressing = [],
-		special_keys = {
-			8: "backspace", 9: "tab", 10: "return", 13: "return", 16: "shift", 17: "ctrl", 91: "cmd", 18: "alt", 19: "pause",
-			20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
-			37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del",
-			96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
-			104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/",
-			112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8",
-			120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 186: "", 191: "/",
-			220: "\\", 222: "'", 224: "cmd", 93: "cmd"
-		}
-
-	this.convert = function(key) {
-
-		if (contains(Object.keys(special_keys),String(key)))
-		{
-			return special_keys[key]
-		} else {
-			return String.fromCharCode(key).toLowerCase()
-		}
-	}
-	this.run = function(key) {
-		var string = this.convert(key.which)
-
-		if(! contains(string, pressing)) {
-			pressing.push(string)
-		}
-
-		pressing.sort()
-	}
-
-	this.capture = function(key_bindings) {
-
-		var keys = Object.keys(key_bindings)
-
-		for(var i=0; i<keys.length; i++) {
-			var key = keys[i],
-				keys_split = key.split(',');
-
-			for(var k=0; k<keys_split.length; k++) {
-				var sorted = keys_split[k].split('_').sort().join('_')
-
-				this.bindings[sorted] = key_bindings[keys[i]]
-			}
-		}
-	}
-
-	this.VERSION = {
-		MAJOR:2,
-		MINOR:1,
-		PATCH:2,
-		FULL :function() {
-			return this.MAJOR+'.'+this.MINOR+'.'+this.PATCH
-		}
-	}
-
-	this.keydown = function(e) {
-		_self.run(e)
-
-		var sorted_pressing    = pressing.sort()
-		var currently_pressing = sorted_pressing.join('_')
-
-		if (contains(Object.keys(_self.bindings),currently_pressing))
-		{
-			_self.bindings[currently_pressing](currently_pressing, e)
-
-			pressing = []
-		}
-	}
-
-	this.keyup = function(e) {
-		var key_string = _self.convert(e.which);
-
-		pressing = pressing.filter(function(value) {
-			return value !== key_string;
-		})
-
-		// The command key behaves extremely
-		// weirdly. It will cause the browser to
-		// 'forget' other keys are depressed and
-		// so the only way to deal with that is a
-		// hack to clear the keys pressed when cmd is
-		// released. Maybe not a correct assumption but
-		// it's better than breaking everything
-		if(key_string == "cmd") {
-			pressing = [];
-		}
-	}
-
-	this.bindTo = function($el) {
-		$el.addEventListener('keydown', this.keydown);
-		$el.addEventListener('keyup', this.keyup);
-	}
-
-	if(typeof $el !== 'undefined') {
+	if($el) {
 		this.bindTo($el);
 	}
 }
+
+Quay.prototype.run = function(key) {
+	var key = this.convert(key.which);
+
+	if(! Quay.utils.contains(this.pressing, key)) {
+		this.pressing.push(key);
+	}
+}
+
+Quay.prototype.keydown = function(e) {
+	this.run(e);
+
+	var currently_pressing = this.pressing.sort().join('_');
+
+	if(Quay.utils.contains(Object.keys(this.bindings), currently_pressing)) {
+		this.bindings[currently_pressing].apply(this, [currently_pressing, e]);
+
+		this.pressing = [];
+	}
+}
+
+Quay.prototype.keyup = function(e) {
+	var key_string = this.convert(e.which);
+
+	var	filterCallback = function(value) {
+		return value !== key_string;
+	}
+
+	// <IE9 doesn't support the
+	// array filter method so Quay.utils
+	// provides a (rather rough) fallback
+
+	var result = (false) ?
+					this.pressing.filter(filterCallback) :
+					Quay.utils.filter(this.pressing, filterCallback);
+
+	this.pressing = result;
+
+	// The command key behaves extremely
+	// weirdly. It will cause the browser to
+	// 'forget' other keys are depressed and
+	// so the only way to deal with that is a
+	// hack to clear the keys pressed when cmd is
+	// released. Maybe not a correct assumption but
+	// it's better than breaking everything
+	if(key_string == "cmd") {
+		this.pressing = [];
+	}
+}
+
+Quay.prototype.bindTo = function(element) {
+	// I'm not keen on how hacky it is to
+	// get the current instance into these
+	// functions (without function.prototype.bind)
+	// but it seem silly to drop <IE8 support for
+	// something like this
+
+	var instance = this;
+
+	element.addEventListener('keydown', function(e) { instance.keydown.apply(instance, [e]) });
+	element.addEventListener('keyup', function(e) { instance.keyup.apply(instance, [e]) });
+}
+
+Quay.utils = {
+	filter: function(haystack, callback) {
+		var result = [];
+
+		for(var i=0; i<haystack.length; i++) {
+			var testedValue = callback.apply(null, [ haystack[i] ])
+
+			if(testedValue) {
+				result.push(haystack[i]);
+			}
+		}
+
+		return result;
+	},
+	contains: function(haystack, needle) {
+		var i = haystack.length;
+
+		while(i--) {
+			if(haystack[i] === needle)
+				return true;
+		}
+
+		return false;
+	}
+}
+
+Quay.prototype.capture = function(key_bindings) {
+	var keys = Object.keys(key_bindings);
+
+	for(var i=0; i<keys.length; i++) {
+		var keys_split = keys[i].split(',');
+
+		for(var k=0; k< keys_split.length; k++) {
+			var sorted = keys_split[k].split('_').sort().join('_');
+
+			this.bindings[sorted] = key_bindings[keys[i]];
+		}
+
+	}
+}
+
+Quay.prototype.convert = function(key) {
+	if(Quay.utils.contains(Object.keys(this.special_keys), String(key))) {
+		return this.special_keys[key];
+	}
+
+	return String.fromCharCode(key).toLowerCase();
+}
+
+// Quay Versioning
+// TODO: Automagic gulp versioning :)
+Quay.VERSION = {
+	MAJOR: 2,
+	MINOR: 1,
+	PATCH: 2,
+	FULL: function() {
+		return [this.MAJOR, this.MINOR, this.PATCH].join('.')
+	}
+}
+
 
 if ( typeof define == "function" && define.amd) {
 	define([], function() {
